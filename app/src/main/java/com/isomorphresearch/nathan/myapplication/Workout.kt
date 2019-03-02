b8fac2399a1e4c801d4a39e663d3dde8013303b3
Cooper Healy <mhealy@mst.edu>
2019-03-02 15:04:05 -0600

    Add workout generation with name and desc

--- app/src/main/java/com/isomorphresearch/nathan/myapplication/Workout.kt
+++ app/src/main/java/com/isomorphresearch/nathan/myapplication/Workout.kt
@@ -2,4 +2,4 @@ package com.isomorphresearch.nathan.myapplication
 
 import java.io.Serializable
 
-class Workout(var exercises: List<Exercise>, var img: String, var name: String) : Serializable
+class Workout(var exercises: List<Exercise>, var desc: String, var img: String, var name: String) : Serializable
