8cfa318b12ac1e3d6e2998ad027089b6d7b19223
Nathan <nawwx3@mst.edu>
2019-03-02 22:39:51 -0600

    Fix input forms

--- app/src/main/java/com/isomorphresearch/nathan/myapplication/ExRecyclerAdapter.kt
+++ app/src/main/java/com/isomorphresearch/nathan/myapplication/ExRecyclerAdapter.kt
@@ -55,7 +55,10 @@ class ExRecyclerAdapter(private var exercises: ArrayList<Exercise>, var context:
         fun bindWorkout(exercise: Exercise) {
             this.exercise = exercise
 //            Picasso.with(view.context).load(workout.img).into(view.itemImage)
-            view.itemDescription.text = exercise.name;
+            view.itemName.text = exercise.name;
+            view.itemDesc.text = exercise.Desc;
+            view.imageView4.setImageResource(R.drawable.ic_logo)
+            view.time.text = exercise.time.toString()
         }
 
 //        companion object {
